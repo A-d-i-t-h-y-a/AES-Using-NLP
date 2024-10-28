90450648f7d75d38a668ed2854a72cd90f20d457
Adithya <periketi.adithyachary@gmail.com>
2024-10-28 08:31:35 +0530

    updated req.txt

--- grader/utils/helpers.py
+++ grader/utils/helpers.py
@@ -4,44 +4,51 @@ import re
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
 import gensim.models.keyedvectors as word2vec
+from transformers import AutoTokenizer
+import spacy
 import math
 
-def essay_to_wordlist(essay_v, remove_stopwords):
-    """Remove the tagged labels and word tokenize the sentence."""
-    essay_v = re.sub("[^a-zA-Z]", " ", essay_v)
-    words = essay_v.lower().split()
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in stops]
-    return (words)
+# Load the spaCy model for English language
+nlp = spacy.load("en_core_web_sm")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-def essay_to_sentences(essay_v, remove_stopwords):
-    """Sentence tokenize the essay and call essay_to_wordlist() for word tokenization."""
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    raw_sentences = tokenizer.tokenize(essay_v.strip())
-    sentences = []
-    for raw_sentence in raw_sentences:
-        if len(raw_sentence) > 0:
-            sentences.append(essay_to_wordlist(raw_sentence, remove_stopwords))
-    return sentences
+def essay_to_wordlist(essay_v, remove_stopwords):
+    """Remove the tagged labels and word tokenize the sentence using spaCy."""
+    doc = nlp(essay_v)
+    words = [token.lemma_ for token in doc if token.is_alpha]  # Get lemmas of words
+    if remove_stopwords:
+        words = [w for w in words if not nlp.vocab[w].is_stop]  # Remove stopwords using spaCy
+    return words
+
+def essay_to_sentences(essay_v):
+    """Sentence tokenize the essay using spaCy."""
+    doc = nlp(essay_v)
+    return [[token.lemma_ for token in sent if token.is_alpha] for sent in doc.sents]
+
+def remove_stopwords(sentences):
+    stops = set(nlp.Defaults.stop_words)
+    filtered_sentences = []
+    for sent in sentences:
+        words = sent.split()  # Split the sentence into words
+        filtered_sentences.append(" ".join([word for word in words if word.lower() not in stops]))
+    return filtered_sentences
 
 def makeFeatureVec(words, model, num_features):
     """Make Feature Vector from the words list of an Essay."""
-    featureVec = np.zeros((num_features,),dtype="float32")
+    featureVec = np.zeros((num_features,), dtype="float32")
     num_words = 0.
     index2word_set = set(model.index_to_key)
     for word in words:
         if word in index2word_set:
             num_words += 1
-            featureVec = np.add(featureVec,model[word])        
-    featureVec = np.divide(featureVec,num_words)
+            featureVec = np.add(featureVec, model[word])
+    if num_words > 0:
+        featureVec = np.divide(featureVec, num_words)
     return featureVec
 
 def getAvgFeatureVecs(essays, model, num_features):
-    """Main function to generate the word vectors for word2vec model."""
-    counter = 0
-    essayFeatureVecs = np.zeros((len(essays),num_features),dtype="float32")
-    for essay in essays:
-        essayFeatureVecs[counter] = makeFeatureVec(essay, model, num_features)
-        counter = counter + 1
+    """Generate the word vectors for Word2Vec model."""
+    essayFeatureVecs = np.zeros((len(essays), num_features), dtype="float32")
+    for idx, essay in enumerate(essays):
+        essayFeatureVecs[idx] = makeFeatureVec(essay, model, num_features)
     return essayFeatureVecs
